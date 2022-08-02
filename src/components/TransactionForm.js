4fa8de0577fdb142736bad043b676f526d906713
David Richard <elidovrich.coding@gmail.com>
2022-08-02 14:53:57 -0400

    add colapsable menu and enlarge button for mobile layout

--- src/components/TransactionForm.js
+++ src/components/TransactionForm.js
@@ -1,25 +1,25 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import {
   addTransaction,
   CATEGORIES,
-} from '../features/transactions/transactionsSlice';
-import { v4 as uuidv4 } from 'uuid';
+} from "../features/transactions/transactionsSlice";
+import { v4 as uuidv4 } from "uuid";
 
 export default function TransactionForm({ categories }) {
   const dispatch = useDispatch();
   const [category, setCategory] = useState(CATEGORIES[0]);
-  const [description, setDescription] = useState('');
+  const [description, setDescription] = useState("");
   const [amount, setAmount] = useState(0);
   const [amountAlert, setAmountAlert] = useState(false);
-
+  const [hide, setHide] = useState(true);
   const handleSubmit = (e) => {
     e.preventDefault();
     //prevent 0 value transaction
-    if(parseFloat(amount) === 0 || !amount) {
+    if (parseFloat(amount) === 0 || !amount) {
       setAmountAlert(true);
       return;
-    };
+    }
     dispatch(
       addTransaction({
         category: category,
@@ -29,15 +29,23 @@ export default function TransactionForm({ categories }) {
       })
     );
     setCategory(CATEGORIES[0]);
-    setDescription('');
+    setDescription("");
     setAmount(0);
   };
-
   return (
     <section className="new-transaction-section">
       <h2>New Transaction</h2>
-      <form onSubmit={handleSubmit}>
-          <div>
+      <div id="hide-btn">
+        <button
+          onClick={() => {!hide ? setHide(true) : setHide(false);}}
+          className={!hide ? "dropdown-btn" : "default-btn"}
+        >
+          {hide ? "New Transaction" : "v"}
+        </button>
+      </div>
+      {!hide && (
+        <form onSubmit={handleSubmit}>
+          <div id="new-transaction-select">
             <label htmlFor="category">Category</label>
             <select
               id="category"
@@ -52,7 +60,7 @@ export default function TransactionForm({ categories }) {
             </select>
           </div>
 
-          <div>
+          <div id="new-transaction-desc">
             <label htmlFor="description">Description</label>
             <input
               id="description"
@@ -62,25 +70,23 @@ export default function TransactionForm({ categories }) {
             />
           </div>
 
-          <div>
+          <div id="new-transaction-amount">
             <label htmlFor="amount">Amount</label>
             <input
               id="amount"
               value={amount}
               onChange={(e) => {
-                setAmount(e.currentTarget.value)
-                setAmountAlert(false)
+                setAmount(e.currentTarget.value);
+                setAmountAlert(false);
               }}
               type="number"
               step="0.01"
               className={amountAlert && "amount-alert"}
             />
           </div>
-        
-
-        <button>Add Transaction</button> 
-      </form>
-      
+          <button id="submit-btn">Add Transaction</button>
+        </form>
+      )}
     </section>
   );
 }
