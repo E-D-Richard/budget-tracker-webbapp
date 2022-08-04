84f4b22082e31280a3385aaeda6b9b47ce93eba0
David Richard <elidovrich.coding@gmail.com>
2022-08-04 18:56:29 -0400

    simulate margin w div and add isDesktop state

--- src/components/TransactionForm.js
+++ src/components/TransactionForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import {
   addTransaction,
@@ -12,7 +12,31 @@ export default function TransactionForm({ categories }) {
   const [description, setDescription] = useState("");
   const [amount, setAmount] = useState(0);
   const [amountAlert, setAmountAlert] = useState(false);
-  const [hide, setHide] = useState(true);
+  const [isDesktop, setDesktop] = useState(window.innerWidth > 750);
+  const [hide, setHide] = useState(!isDesktop ? true : false);
+
+  const updateScreenWidth = () => {
+    setDesktop(window.innerWidth > 750);
+    if(window.innerWidth > 750){
+      setHide(false);
+    } else {
+      setHide(true);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateScreenWidth);
+    return () => window.removeEventListener("resize", updateScreenWidth);
+  });
+
+  const handleHideForm = () => {
+    if (!isDesktop && !hide){
+      setHide(true);
+    } else {
+      setHide(false)
+    }
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     //prevent 0 value transaction
@@ -33,60 +57,63 @@ export default function TransactionForm({ categories }) {
     setAmount(0);
   };
   return (
-    <section className="new-transaction-section">
-      <h2>New Transaction</h2>
-      <div id="hide-btn">
-        <button
-          onClick={() => {!hide ? setHide(true) : setHide(false);}}
-          className={!hide ? "dropdown-btn" : "default-btn"}
-        >
-          {hide ? "New Transaction" : "v"}
-        </button>
-      </div>
-      {!hide && (
-        <form onSubmit={handleSubmit}>
-          <div id="new-transaction-select">
-            <label htmlFor="category">Category</label>
-            <select
-              id="category"
-              value={category}
-              onChange={(e) => setCategory(e.currentTarget.value)}
-            >
-              {CATEGORIES.map((c) => (
-                <option key={c} value={c}>
-                  {c}
-                </option>
-              ))}
-            </select>
-          </div>
+    <div className="new-transaction-container">
+      <div id={isDesktop ? "mock-desktop-margin": `${!hide ? "mock-expanded-margin": "mock-collapsed-margin"}`}></div>
+      <section >
+        <h2>New Transaction</h2>
+        <div id="hide-btn">
+          <button
+            onClick={handleHideForm}
+            className={!hide ? "dropdown-btn" : "default-btn"}
+          >
+            {hide ? "New Transaction" : "v"}
+          </button>
+        </div>
+        {!hide && (
+          <form onSubmit={handleSubmit}>
+            <div id="new-transaction-select">
+              <label htmlFor="category">Category</label>
+              <select
+                id="category"
+                value={category}
+                onChange={(e) => setCategory(e.currentTarget.value)}
+              >
+                {CATEGORIES.map((c) => (
+                  <option key={c} value={c}>
+                    {c}
+                  </option>
+                ))}
+              </select>
+            </div>
 
-          <div id="new-transaction-desc">
-            <label htmlFor="description">Description</label>
-            <input
-              id="description"
-              value={description}
-              onChange={(e) => setDescription(e.currentTarget.value)}
-              type="text"
-            />
-          </div>
+            <div id="new-transaction-desc">
+              <label htmlFor="description">Description</label>
+              <input
+                id="description"
+                value={description}
+                onChange={(e) => setDescription(e.currentTarget.value)}
+                type="text"
+              />
+            </div>
 
-          <div id="new-transaction-amount">
-            <label htmlFor="amount">Amount</label>
-            <input
-              id="amount"
-              value={amount}
-              onChange={(e) => {
-                setAmount(e.currentTarget.value);
-                setAmountAlert(false);
-              }}
-              type="number"
-              step="0.01"
-              className={amountAlert && "amount-alert"}
-            />
-          </div>
-          <button id="submit-btn">Add Transaction</button>
-        </form>
-      )}
-    </section>
+            <div id="new-transaction-amount">
+              <label htmlFor="amount">Amount</label>
+              <input
+                id="amount"
+                value={amount}
+                onChange={(e) => {
+                  setAmount(e.currentTarget.value);
+                  setAmountAlert(false);
+                }}
+                type="number"
+                step="0.01"
+                className={amountAlert ? "amount-alert" : ""}
+              />
+            </div>
+            <button id="submit-btn">Add Transaction</button>
+          </form>
+        )}
+      </section>
+    </div>
   );
 }
