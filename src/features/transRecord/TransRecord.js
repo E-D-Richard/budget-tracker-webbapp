23c97c210ecb60d11b0099ce13f31388f53dccf6
David Richard <elidovrich.coding@gmail.com>
2023-12-14 20:58:58 -0500

    beautify

--- src/features/transRecord/TransRecord.js
+++ src/features/transRecord/TransRecord.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { selectFlattenedTransactions } from './transRecordSlice';
-import TransactionList from '../../components/body/TransactionList';
-import { selectBudgets } from '../budgets/budgetsSlice';
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectFlattenedTransactions } from "./transRecordSlice";
+import TransactionList from "../../components/body/TransactionList";
+import { selectBudgets } from "../budgets/budgetsSlice";
 
 const TransRecord = () => {
   const expenseTransactions = useSelector(selectFlattenedTransactions);
-  const budgets = useSelector(selectBudgets)
-  const budgetTransactions = []
-  for(const budget of budgets){
-    if(budget.history.length > 0){
-      budgetTransactions.push(...budget.history)
+  const budgets = useSelector(selectBudgets);
+  const budgetTransactions = [];
+  for (const budget of budgets) {
+    if (budget.history.length > 0) {
+      budgetTransactions.push(...budget.history);
     }
   }
 
   //console.log(allTransactions);
 
-
-  
-
   return (
-      <TransactionList expenseTransactions={expenseTransactions} budgetTransactions={budgetTransactions} />
+    <TransactionList
+      expenseTransactions={expenseTransactions}
+      budgetTransactions={budgetTransactions}
+    />
   );
 };
 
