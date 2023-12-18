2f2b0ab0789baa9e27b68ad9ce552a3f97f4b647
David Richard <elidovrich.coding@gmail.com>
2023-12-18 15:17:58 -0500

    set menuIsOpen default to false

--- src/components/header/settings/Settings.js
+++ src/components/header/settings/Settings.js
@@ -4,7 +4,7 @@ import "./settings.css";
 import { useState } from "react";
 
 const Settings = () => {
-  const [menuIsOpen, setMenuIsOpen] = useState(true);
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
   return (
     <div id="settings" className={menuIsOpen ? "open" : ""}>
       <button id="settings-button" onClick={()=>setMenuIsOpen(!menuIsOpen)}>
