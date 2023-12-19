2ef6b8c2db6256d219a9a23fcb797ded35b5fd74
David Richard <elidovrich.coding@gmail.com>
2023-12-19 16:54:10 -0500

    make custom-settings dissapear behind button when overflow scrolling

--- src/components/header/settings/Settings.js
+++ src/components/header/settings/Settings.js
@@ -6,10 +6,12 @@ import { useState } from "react";
 const Settings = () => {
   const [menuIsOpen, setMenuIsOpen] = useState(false);
   return (
-    <div id="settings-container" className={menuIsOpen ? "open" : ""} >
-      <button id="settings-button" onClick={() => setMenuIsOpen(!menuIsOpen)}>
-        <AiFillSetting />
-      </button>
+    <div id="settings-container" className={menuIsOpen ? "open" : ""}>
+      <div className="button-container">
+        <button id="settings-button" onClick={() => setMenuIsOpen(!menuIsOpen)}>
+          <AiFillSetting />
+        </button>
+      </div>
       <div id="settings" className={menuIsOpen ? "open" : ""}>
         <CustomizeCategories menuIsOpen={menuIsOpen} />
         <p>random text</p>
