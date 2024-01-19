08118e488378425234542ee3b8844e0d5df2c981
David Richard <elidovrich.coding@gmail.com>
2024-01-19 01:47:22 -0500

    pause mobile table headers

--- src/components/body/transactions/transaction/TransHeaders.js
+++ src/components/body/transactions/transaction/TransHeaders.js
@@ -1,18 +1,18 @@
 const TransHeaders = ({isMobile, listType}) => {
 
-  if(isMobile){
-    return (
-      <tr>
-      <td className="t-header description">Desc</td>
-      <td className="t-header amount">Amnt</td>
-      {/* <td className="t-header prev-budget-total">Prev <br /> Bdgt <br/> Ttl</td>
-      <td className="t-header current-budget-total">Cur <br /> Bdgt <br /> Ttl</td>
-      <td className="t-header expenses-total">Exps Total</td> */}
-      {listType === "expense" ? <td className="t-header note">Note</td> :""}
-      <td className="t-header delete"></td>
-    </tr>
-    )
-  }
+  // if(isMobile){
+  //   return (
+  //     <tr>
+  //     <td className="t-header description">Desc</td>
+  //     <td className="t-header amount">Amnt</td>
+  //     {/* <td className="t-header prev-budget-total">Prev <br /> Bdgt <br/> Ttl</td>
+  //     <td className="t-header current-budget-total">Cur <br /> Bdgt <br /> Ttl</td>
+  //     <td className="t-header expenses-total">Exps Total</td> */}
+  //     {listType === "expense" ? <td className="t-header note">Note</td> :""}
+  //     <td className="t-header delete"></td>
+  //   </tr>
+  //   )
+  // }
   return (
     <tr>
       <td className="t-header description">Description</td>
