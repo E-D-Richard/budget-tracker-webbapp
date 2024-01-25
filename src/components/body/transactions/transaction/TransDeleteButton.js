0413856bee8ee29bd2ad838ea89b64563d343a7a
David Richard <elidovrich.coding@gmail.com>
2024-01-25 02:16:07 -0500

    remove netlify deployment errors

--- src/components/body/transactions/transaction/TransDeleteButton.js
+++ src/components/body/transactions/transaction/TransDeleteButton.js
@@ -1,13 +1,11 @@
-import { v4 as uuidv4 } from "uuid";
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { addBudgetBalanceEntry, deleteBudgetTransaction, selectBudgets, updateBudgetTotalsBasedOnNewlyDeletedExpense } from "../../../../features/budgets/budgetsSlice";
-import { deleteExpenseTransaction, selectTransactions } from "../../../../features/transRecords/transRecordsSlice";
+import { useDispatch } from "react-redux";
+import { deleteBudgetTransaction, updateBudgetTotalsBasedOnNewlyDeletedExpense } from "../../../../features/budgets/budgetsSlice";
+import { deleteExpenseTransaction } from "../../../../features/transRecords/transRecordsSlice";
 
 const TransDeleteButton = ({ transaction }) => {
   const dispatch = useDispatch();
-  const selectedCategoryTotalExpenses = useSelector(selectTransactions)[transaction.category].total;
-  const currentBudgetTotal = useSelector(selectBudgets)[transaction.category].currentTotal;
+
   const handleDelete = (e) => {
     if (transaction.type === "expense") {
       dispatch(deleteExpenseTransaction(transaction));
